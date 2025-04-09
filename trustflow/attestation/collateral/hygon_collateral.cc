145010cfcf05d083fafe8221680477b949aba89c
zhongtianq <zhongtian.qzt@antgroup.com>
2025-04-09 10:48:12 +0800

    revert and fix hygon cert fetch

--- trustflow/attestation/collateral/hygon_collateral.cc
+++ trustflow/attestation/collateral/hygon_collateral.cc
@@ -14,6 +14,7 @@
 
 #include "trustflow/attestation/collateral/hygon_collateral.h"
 
+#include "brpc/channel.h"
 #include "cppcodec/base64_rfc4648.hpp"
 #include "csv/attestation/attestation.h"
 #include "spdlog/spdlog.h"
@@ -21,18 +22,14 @@
 
 #include "trustflow/attestation/utils/json2pb.h"
 
-#define CPPHTTPLIB_OPENSSL_SUPPORT
-#include "httplib.h"
-
 namespace trustflow {
 namespace attestation {
 namespace collateral {
 
 namespace {
+constexpr int kHttpTimeoutMs = 500;
 
 constexpr int kHttpRetryTimes = 3;
-
-constexpr int kHttpResponseOk = 200;
 
 constexpr char kHygonCertUrl[] = "https://cert.hygon.cn";
 
@@ -44,33 +41,32 @@ constexpr char kHygonHskCekSubUrl[] = "/hsk_cek?snumber=";
 void GetHygonCsvCollateral(
     const std::string &chip_id,
     secretflowapis::v2::sdc::HygonCsvCertChain &hygon_csv_cert_chain) {
-  httplib::Client client(kHygonCertUrl);
-  // TODO support server certificate verification
-  client.enable_server_certificate_verification(false);
+  brpc::Channel channel;
+  brpc::ChannelOptions options;
+  options.protocol = brpc::ProtocolType::PROTOCOL_HTTP;
+  options.timeout_ms = kHttpTimeoutMs;
+  options.max_retry = kHttpRetryTimes;
+  YACL_ENFORCE_EQ(channel.Init(kHygonCertUrl, &options), 0,
+                  "hygon cert server channel init failed");
 
-  std::string hsk_cek_url = std::string(kHygonHskCekSubUrl) + chip_id;
+  brpc::Controller cntl;
+  cntl.http_request().uri() = kHygonHskCekSubUrl + chip_id;
+  cntl.http_request().set_method(brpc::HTTP_METHOD_GET);
+  channel.CallMethod(nullptr, &cntl, nullptr, nullptr, nullptr);
+  YACL_ENFORCE(!cntl.Failed(),
+               "Get hygon csv hsk and cek failed, error code: {}, message: {}",
+               cntl.ErrorCode(), cntl.ErrorText());
+  const std::string response_body = cntl.response_attachment().to_string();
 
-  httplib::Result ret{nullptr, httplib::Error::Unknown};
-  for (int i = 0; i < kHttpRetryTimes; i++) {
-    ret = client.Get(hsk_cek_url);
-    if (ret && ret->status == kHttpResponseOk) {
-      break;
-    }
-  }
-  YACL_ENFORCE(ret, "Failed to connect hygon cert server, http ret is null");
-  YACL_ENFORCE(ret->status == kHttpResponseOk,
-               "Get hygon csv hsk and cek failed, http status: {}, reason: {}",
-               ret->status, ret->reason);
-
-  // ret->body is a binary string
+  // response_body is a binary string
   YACL_ENFORCE_EQ(
-      ret->body.length(), sizeof(CHIP_ROOT_CERT_t) + sizeof(CSV_CERT_t),
+      response_body.length(), sizeof(CHIP_ROOT_CERT_t) + sizeof(CSV_CERT_t),
       "hsk and cek length should be {}, but got {}",
-      sizeof(CHIP_ROOT_CERT_t) + sizeof(CSV_CERT_t), ret->body.length());
+      sizeof(CHIP_ROOT_CERT_t) + sizeof(CSV_CERT_t), response_body.length());
   hygon_csv_cert_chain.set_b64_hsk_cert(cppcodec::base64_rfc4648::encode(
-      ret->body.data(), sizeof(CHIP_ROOT_CERT_t)));
+      response_body.data(), sizeof(CHIP_ROOT_CERT_t)));
   hygon_csv_cert_chain.set_b64_cek_cert(cppcodec::base64_rfc4648::encode(
-      ret->body.data() + sizeof(CHIP_ROOT_CERT_t), sizeof(CSV_CERT_t)));
+      response_body.data() + sizeof(CHIP_ROOT_CERT_t), sizeof(CSV_CERT_t)));
 
   SPDLOG_INFO("Get hygon csv hsk and cek from hygon website succeed");
 }
