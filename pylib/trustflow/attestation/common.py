c437518e89e69913d1caa63f6efd990ef97c98b6
oeqqwq <ruidong.qrd@antgroup.com>
2025-09-18 17:04:12 +0800

    update header

--- pylib/trustflow/attestation/common.py
+++ pylib/trustflow/attestation/common.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 from dataclasses import dataclass, fields
 from typing import Optional, List, Any
@@ -32,7 +46,7 @@ def _filter_none_values(obj: Any) -> Any:
 @dataclass
 class AttestationGenerationParams:
     """Parameters for generating attestation reports.
-    
+
     Maps to UnifiedAttestationGenerationParams in ual.proto
     """
 
@@ -62,7 +76,7 @@ class AttestationGenerationParams:
 @dataclass
 class AttestationReportParams:
     """Special parameters for different TEE platforms.
-    
+
     Maps to UnifiedAttestationReportParams in ual.proto
     """
 
@@ -92,7 +106,7 @@ class AttestationReportParams:
 @dataclass
 class AttestationReport:
     """Unified attestation report.
-    
+
     Maps to UnifiedAttestationReport in ual.proto
     """
 
@@ -122,7 +136,7 @@ class AttestationReport:
 @dataclass
 class AttestationAttribute:
     """Collects all attributes information of trusted application.
-    
+
     Maps to UnifiedAttestationAttributes in ual.proto
     """
 
@@ -180,7 +194,7 @@ class AttestationAttribute:
 @dataclass
 class AttestationPolicy:
     """Policy used when verifying the attestation report.
-    
+
     Maps to UnifiedAttestationPolicy in ual.proto
     """
 
@@ -214,7 +228,7 @@ class AttestationPolicy:
 @dataclass
 class AttestationNestedPolicies:
     """Match rules for nested reports verification.
-    
+
     Maps to UnifiedAttestationNestedPolicies in ual.proto
     """
 
@@ -245,7 +259,7 @@ class AttestationNestedPolicies:
 @dataclass
 class AttestationNestedPolicy:
     """Match rules for nested report verification.
-    
+
     Maps to UnifiedAttestationNestedPolicy in ual.proto
     """
 
