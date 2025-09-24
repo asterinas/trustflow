9c524a4777fe63a0a141d5d4770c25582a3ebbcb
Joe <ruidong.qrd@antgroup.com>
2025-09-24 16:23:29 +0800

    fix module path error (#110)

--- pylib/trustflow/attestation/generation/api.py
+++ pylib/trustflow/attestation/generation/api.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pylib.trustflow.attestation.common import (
+from .. import (
     AttestationGenerationParams,
     AttestationReport,
 )
